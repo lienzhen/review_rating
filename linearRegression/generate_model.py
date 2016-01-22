529eb0c83fcf654df40ea26891f06ff7bd2f41d8
lienzhen <1024897818@qq.com>
2016-01-22 19:38:49 +0800

    add sparse

--- linearRegression/generate_model.py
+++ linearRegression/generate_model.py
@@ -1,15 +1,22 @@
 #coding = "utf-8"
 from sklearn.linear_model import LinearRegression
-import random
 from sklearn.externals import joblib
 import numpy as np
 import os
 from datetime import datetime
+import scipy.sparse as sp
+from gensim import corpora
 
 def logging(logstr):
     print "%s\t%s" % (datetime.now(), logstr)
 
-def load_trainingData(train_file, train_value_file):
+def get_len_vector():
+    model_directory = "../../paper/data/dianping/tfidf/model"
+    tfidf_loadDic = os.path.join(model_directory, "tfidf.dic")
+    dictionary = corpora.Dictionary.load(tfidf_loadDic)
+    return len(dictionary)
+
+def load_trainingData(train_file):
     starttime = datetime.now()
     train_x = []
     train_y = []
@@ -22,25 +29,45 @@ def load_trainingData(train_file, train_value_file):
             if index  % 200 == 0:
                 logging("%d cases" % index)
             arr = line.strip().split("\t")
-            vec_list = arr[2:]
+            vec_list = arr[2:-1]
             vec = [float(v) for v in vec_list]
             train_x.append(vec)
+            train_y.extend([float(arr[-1])])
+    logging("loading training data, eplased time:%s" % str(datetime.now() - starttime))
+    return np.array(train_x), np.array(train_y)
 
-    logging("loading training value data")
-    with open(train_value_file) as f:
-        index = 0
+def load_sparse_trainingData(train_file, col_num):
+    starttime = datetime.now()
+    #train_x = []
+    train_y = []
+    rows = []
+    cols = []
+    data = []
+    row_num = 0
+
+    index = 0
+    logging("loading training data")
+    with open(train_file) as f:
         for line in f:
             index += 1
             if index  % 200 == 0:
                 logging("%d cases" % index)
             arr = line.strip().split("\t")
-            if len(arr) != 3:continue
-            train_y.extend([float(arr[2])])
+            train_y.extend([float(arr[-1])])
+            for each in arr[2:-1]:
+                col_index, value = each.strip().split(',')
+                col_index = int(col_index)
+                rows.append(row_num)
+                cols.append(col_index)
+                data.append(float(value))
+            row_num += 1
     logging("loading training data, eplased time:%s" % str(datetime.now() - starttime))
-    return np.array(train_x), np.array(train_y)
+    return sp.coo_matrix((np.array(data), (np.array(rows), np.array(cols))), shape=(row_num, col_num)), np.array(train_y)
 
-def main(train_file, train_value_file, model_file):
-    train_x, train_y = load_trainingData(train_file, train_value_file)
+def main(train_file, model_file):
+    train_x, train_y = load_sparse_trainingData(train_file, 2 * get_len_vector())
+    logging('len of y: %d' % train_y.shape)
+    logging(train_x.shape)
     LR = LinearRegression()
     logging("training model...")
     starttime = datetime.now()
@@ -51,9 +78,7 @@ def main(train_file, train_value_file, model_file):
 
 if __name__ == "__main__":
     training_file_directory = "../../paper/data/dianping/tfidf/vector/"
-    training_value_directory = "../../paper/data/dianping/corpus/"
     train_data = os.path.join(training_file_directory, "comment.keyword.train.joint.vector.1000")
-    train_value = os.path.join(training_value_directory, "comment.keyword.train.residual.1000")
     model_directory = "../../paper/data/dianping/lr_model/"
-    model_file = os.path.join(model_directory, "tfidf.1000")
-    main(train_data, train_value, model_file)
+    model_file = os.path.join(model_directory, "tfidf_top10K")
+    main(train_data,model_file)
---
+++ linearRegression/predict.py
@@ -0,0 +1,73 @@
+#coding = "utf-8"
+from sklearn.linear_model import LinearRegression
+from sklearn.externals import joblib
+import numpy as np
+import os
+from datetime import datetime
+import scipy.sparse as sp
+from generate_model import get_len_vector
+
+def logging(logstr):
+    print "%s\t%s" % (datetime.now(), logstr)
+
+logging("loading lr model")
+model_directory = "../../paper/data/dianping/lr_model/"
+model_file = os.path.join(model_directory, "tfidf_top10K")
+lr_model = joblib.load(model_file)
+
+def trans_tfidf_to_sparse(vec, col_size):
+    #vec format
+    #[(col, value), (col, value), ...]
+    rows = []
+    cols = []
+    data = []
+    for each in vec:
+        rows.append(1)
+        cols.append(int(each[0]))
+        data.append(float(each[1]))
+    return sp.coo_matrix((np.array(data), (np.array(rows), np.array(cols))), shape=(2, col_size))
+
+def trans_tfidf_to_dense(vec, col_size):
+    data = [0.0] * col_size
+    for index, value in vec:
+        data[index] = value
+    return data
+
+def parse_value(x):
+    arr = x.split(',')
+    return (int(arr[0]), float(arr[1]))
+
+def load_train_vec(file_name):
+    dic = {}
+    logging("loading %s vetor" % file_name)
+    with open(file_name) as f:
+        for line in f:
+            line = line.strip().split("\t")
+            #map to [(int, float), ...]
+            dic[line[0]] = map(parse_value, line[1:])
+            #vec_list = line[1:]
+            #dic[line[0]] = [item.split(",") for item in vec_list]
+    return dic
+
+vector_directory = "../../paper/data/dianping/tfidf/vector"
+user_vector = os.path.join(vector_directory, "comment.keyword.train.user.vector.1000")
+shop_vector = os.path.join(vector_directory, "comment.keyword.train.shop.vector.1000")
+
+user_vec_dic = load_train_vec(user_vector)
+shop_vec_dic = load_train_vec(shop_vector)
+#for i in shop_vec_dic.keys():
+    #print shop_vec_dic[i]
+
+def lr_predict(uid, sid):
+    u_vec = []
+    s_vec = []
+    if user_vec_dic.has_key(uid):
+        u_vec = user_vec_dic[uid]
+    if shop_vec_dic.has_key(sid):
+        s_vec = shop_vec_dic[sid]
+    #both sparse matrix and dense matrix are passed test
+    #dense matrix is more efficient here
+    #joint_vec = trans_tfidf_to_sparse(u_vec + s_vec, 2 * get_len_vector())
+    joint_vec = trans_tfidf_to_dense(u_vec + s_vec, 2 * get_len_vector())
+    result = lr_model.predict(joint_vec)
+    return result
