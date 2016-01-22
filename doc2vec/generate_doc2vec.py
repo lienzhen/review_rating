8eccfe23ff702983e96a56496bd892b5325585a3
lienzhen <1024897818@qq.com>
2016-01-22 11:16:25 +0800

    update doc2vec

--- doc2vec/generate_doc2vec.py
+++ doc2vec/generate_doc2vec.py
@@ -1,9 +1,20 @@
-from gensim.models.doc2vec import LabeledSentence, Doc2Vec
-from collections import defaultdict
-import csv
-import jieba
+#coding = utf-8
 import sys
+sys.path.append("../langconv/")
+from langconv import *
 from datetime import datetime
+import os
+import jieba
+from gensim.models.doc2vec import LabeledSentence, Doc2Vec
+
+stop_words = [word.strip() for word in open("../../paper/data/dianping/stopwords.txt").readlines()]
+
+def isnumeric(num):
+    try:
+        float(num)
+        return True
+    except:
+        return False
 
 def log(logstr, writer = sys.stdout):
     writer.write("%s\t%s\n" % (str(datetime.now()), logstr))
@@ -13,13 +24,6 @@ def log_inline(logstr, writer = sys.stdout):
     writer.write("%s\t%s\r" % (str(datetime.now()), logstr))
     writer.flush()
 
-def load_file_set(filename):
-    res = set()
-    with open(filename) as fin:
-        for line in fin:
-            res.add(line.strip().decode('utf-8'))
-    return res
-
 def load_user_comment(filename):
     comment_labeled = []
     log('loading LabeledSentence...')
@@ -27,49 +31,46 @@ def load_user_comment(filename):
     with open(filename) as fin:
         for line in fin:
             line = line.strip().split('\t')
-            arr = [x for x in line if x.strip() != ""]
-            comment_labeled.append(LabeledSentence(words=arr[1:], tags=[arr[0].decode('utf-8')]))
+            if len(line) < 2:continue
+            content = ' '.join(line[1:])
+            content = Converter('zh-hans').convert(content.decode("utf-8")).encode("utf-8")
+            arr = []
+            for word in jieba.cut(content):
+                if word.strip():
+                    w = word.strip().lower().encode("utf-8")
+                    if w not in stop_words and not isnumeric(w):
+                        arr.extend([w])
+            #arr = [x for x in line if x.strip() != ""]
+            #comment_labeled.append(LabeledSentence(words=arr[1:], tags=[arr[0].decode('utf-8')]))
+            comment_labeled.append(LabeledSentence(words=arr, tags=[line[0].decode('utf-8')]))
             log(count)
             count += 1
     print ''
     return comment_labeled
 
 def main():
-    #user_comment = load_user_comment('../../paper/data/user_comment_without_stopword.txt')
-    user_comment = load_user_comment('../../paper/data/shop_comment_dish.txt')
-    log('training model...')
+    data_directory = "../../paper/data/dianping/corpus/"
+    vector_directory = "../../paper/data/dianping/p2v/"
+    #data_user  = os.join.path(data_directory, "comment.keyword.train.user")
+    data_user  = os.path.join(data_directory, "comment.keyword.train.user.1000")
+    data_shop  = os.path.join(data_directory, "comment.keyword.train.shop.1000")
+    vector_user = os.path.join(vector_directory, "comment.keyword.train.user.vector")
+    vector_shop = os.path.join(vector_directory, "comment.keyword.train.shop.vector")
+
+    user_comment = load_user_comment(data_user)
+    log('training user model...')
     model = Doc2Vec(user_comment, size=100)
-    log('saving model...')
-    model.save('../model/shop.d2v.dish.model')
+    log('saving user model...')
+    #model.save(vector_user)
+    model.save_word2vec_format(vector_user, binary=False)
+
+    shop_comment = load_user_comment(data_shop)
+    log('training shop model...')
+    model = Doc2Vec(shop_comment, size=100)
+    log('saving shop model...')
+    model.save(vector_shop)
+
 
 if __name__ == '__main__':
     main()
-
-def _load_user_comment(filename):
-    #stop_words = load_file_set('../../paper/data/full_stopwords.txt')
-    stop_words = set()
-    fout = file('../../paper/data/shop_comment.txt', 'w')
-    user_comment = defaultdict(list)
-    log('reading comments...')
-    count = 1
-    with open(filename) as fin:
-        reader = csv.DictReader(fin)
-        for row in reader:
-            user_comment[row['shop_id']].append(row['content'])
-            sys.stdout.write('\r%d' % count)
-            count += 1
-    log('loading LabeledSentence...')
-    comment_labeled = []
-    count = 1
-    for user_id, comments in user_comment.iteritems():
-        words = []
-        for sentence in comments:
-            words.extend([x for x in jieba.cut(sentence) if x not in stop_words])
-        #comment_labeled.append(LabeledSentence(words=words, tags=[('uid_' + user_id).decode('utf-8')]))
-        sys.stdout.write('\r%d' % count)
-        count += 1
-        fout.write('%s\t%s\n' % (user_id, '\t'.join([x.encode('utf-8') for x in words])))
-    fout.close()
-    return comment_labeled
-
 
