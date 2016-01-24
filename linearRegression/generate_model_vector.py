f716f9daed2f3c68be1dd44e3050c4cc79bf5f32
lienzhen <1024897818@qq.com>
2016-01-24 14:31:08 +0800

    add w2v model code

---
+++ linearRegression/generate_model_vector.py
@@ -0,0 +1,76 @@
+#coding = "utf-8"
+import sys
+sys.path.append('../tfidf/')
+from sklearn.linear_model import LinearRegression
+from sklearn.externals import joblib
+import numpy as np
+import os
+from datetime import datetime
+import scipy.sparse as sp
+from gensim import corpora
+from joint_vec import load_vec
+
+def logging(logstr):
+    print "%s\t%s" % (datetime.now(), logstr)
+
+def load_vec(filename):
+    vec = {}
+    logging("loading %s vector" % filename)
+    starttime = datetime.now()
+    with open(filename) as f:
+        for line in f:
+            arr = line.strip().split("\t")
+            vec[arr[0]] = arr[1:]
+    logging("loading %s vector, eplased time:%s" % (filename, str(datetime.now() - starttime)))
+    return vec
+
+vector_file_directory = "../../paper/data/dianping/w2v/vector/"
+train_user = os.path.join(vector_file_directory, "comment.keyword.train.user.vector")
+train_shop = os.path.join(vector_file_directory, "comment.keyword.train.shop.vector")
+user_vec = load_vec(train_user)
+shop_vec = load_vec(train_shop)
+
+def load_trainingData(train_file):
+    starttime = datetime.now()
+    train_x = []
+    train_y = []
+
+    index = 0
+    logging("loading training data")
+    with open(train_file) as f:
+        for line in f:
+            index += 1
+            if index  % 2000 == 0:
+                logging("%d cases" % index)
+            arr = line.strip().split("\t")
+            if len(arr) != 3:
+                continue
+            if not user_vec.has_key(arr[0]) or not shop_vec.has_key(arr[1]):continue
+            u_vec = user_vec[arr[0]]
+            s_vec = shop_vec[arr[1]]
+            vec_list = u_vec + s_vec
+            vec = [float(v) for v in vec_list]
+            train_x.append(vec)
+            train_y.extend([float(arr[-1])])
+    logging("loading training data, eplased time:%s" % str(datetime.now() - starttime))
+    return np.array(train_x), np.array(train_y)
+
+
+def main(train_file, model_file):
+    train_x, train_y = load_trainingData(train_file)
+    LR = LinearRegression()
+    logging("training model...")
+    starttime = datetime.now()
+    LR.fit(train_x, train_y)
+    logging("training model, eplased time:%s" % str(datetime.now() - starttime))
+    logging("saving model")
+    joblib.dump(LR, model_file)
+
+if __name__ == "__main__":
+    data_directory = "../../paper/data/dianping/corpus/"
+    data = os.path.join(data_directory, "comment.keyword.train.residual")
+
+    model_directory = "../../paper/data/dianping/lr_model/"
+    #model_file = os.path.join(model_directory, "tfidf_top10K")
+    model_file = os.path.join(model_directory, "w2v_500")
+    main(data, model_file)
--- linearRegression/predict.py
+++ linearRegression/predict.py
@@ -12,8 +12,8 @@ def logging(logstr):
 
 logging("loading lr model")
 model_directory = "../../paper/data/dianping/lr_model/"
-model_file = os.path.join(model_directory, "tfidf_top10K")
-#model_file = os.path.join(model_directory, "w2v_500")
+#model_file = os.path.join(model_directory, "tfidf_top10K")
+model_file = os.path.join(model_directory, "w2v_500")
 lr_model = joblib.load(model_file)
 
 def trans_tfidf_to_sparse(vec, col_size):
@@ -63,29 +63,36 @@ def load_vector(file_name):
             #dic[line[0]] = [float(x) for x in vec_list]
     return dic
 
-vector_directory = "../../paper/data/dianping/tfidf/vector"
-#vector_directory = "../../paper/data/dianping/w2v/vector"
-user_vector = os.path.join(vector_directory, "comment.keyword.train.user.vector.1000")
-shop_vector = os.path.join(vector_directory, "comment.keyword.train.shop.vector.1000")
+#vector_directory = "../../paper/data/dianping/tfidf/vector"
+vector_directory = "../../paper/data/dianping/w2v/vector"
+user_vector = os.path.join(vector_directory, "comment.keyword.train.user.vector")
+shop_vector = os.path.join(vector_directory, "comment.keyword.train.shop.vector")
+#user_vector = os.path.join(vector_directory, "comment.keyword.train.user.vector.1000")
+#shop_vector = os.path.join(vector_directory, "comment.keyword.train.shop.vector.1000")
 
-user_vec_dic = load_train_vec(user_vector)
-shop_vec_dic = load_train_vec(shop_vector)
-#user_vec_dic = load_vector(user_vector)
-#shop_vec_dic = load_vector(shop_vector)
+#user_vec_dic = load_train_vec(user_vector)
+#shop_vec_dic = load_train_vec(shop_vector)
+user_vec_dic = load_vector(user_vector)
+shop_vec_dic = load_vector(shop_vector)
 #for i in shop_vec_dic.keys():
     #print shop_vec_dic[i]
 
 def lr_predict(uid, sid):
     u_vec = []
     s_vec = []
-    if user_vec_dic.has_key(uid):
-        u_vec = user_vec_dic[uid]
-    if shop_vec_dic.has_key(sid):
-        s_vec = shop_vec_dic[sid]
+    if not user_vec_dic.has_key(uid) or not shop_vec_dic.has_key(sid):return 0
+#    if user_vec_dic.has_key(uid):
+    u_vec = user_vec_dic[uid]
+  #  if shop_vec_dic.has_key(sid):
+    s_vec = shop_vec_dic[sid]
     #both sparse matrix and dense matrix are passed test
     #dense matrix is more efficient here
     #joint_vec = trans_tfidf_to_sparse(u_vec + s_vec, 2 * get_len_vector())
-    joint_vec = trans_tfidf_to_dense(u_vec + s_vec, 2 * get_len_vector())
-    #joint_vec = u_vec + s_vec
+    #joint_vec = trans_tfidf_to_dense(u_vec + s_vec, 2 * get_len_vector())
+    #if len(u_vec) == 0:
+     #   u_vec = len(s_vec) * [0.0]
+    #if len(s_vec) == 0:
+     #   s_vec = len(u_vec) * [0.0]
+    joint_vec = u_vec + s_vec
     result = lr_model.predict(joint_vec)
     return result
